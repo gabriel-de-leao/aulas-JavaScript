ae80b67ee4fe0487778df5617ce2353515c8014d
Gabriel de Leão <gabriel_de-leao@estudante.sesisenai.org.br>
2026-09-09 22:16:13 -0300

    Sistema de cadastro de itens-JS

--- atividades.js/atividade.js
+++ atividades.js/atividade.js
@@ -1,12 +1,80 @@
-const nomeItem = ['Armadura de titânio', 'Poção de cura', 'Elixir da vida', 'Anel da invisibilidade']
+const nomeItem = []
 const precoItem = []
-const raridade = []
-const estoque = []
+let raridade = []
+let estoque = []
 const prompt = require(`prompt-sync`)()
+const escolha = []
+let id = []
+const finalVetor = 100
 // Mostra os itens que estão disponíveis no catálogo da loja
-const catalogo_itens = ['Armadura de titânio', 'Poção de cura', 'Elixir da vida', 'Anel da invisibilidade']
+const catalogo_itens = ['Espada de fogo rasga céus', 'Armadura de titânio', 'Poção de cura', 'Elixir da vida', 'Anel da invisibilidade']
 
-for (let i = 0; i < 4; i++) {  // Pede para o usuário digitar o preço e a quantidade em estoque de cada item
+function Vazio () {
+    let espacoVazio = -1;
+    for (let i = 0; i < nomeItem.length; i++) {
+        if (!(i in nomeItem)){
+            espacoVazio = i;
+            break;
+        }
+    }
+    if (espacoVazio === -1) {
+        espacoVazio = nomeItem.length;
+
+
+    }
+
+    return espacoVazio;
+
+}
+
+function telaInicial () {
+    console.clear()
+
+    console.log (`|----------------------------------------------------|`)
+    console.log (`|                  Menu Principal                    |`)
+    console.log (`|   1 - Cadastro                                     |`)
+    console.log (`|   2 - Listagem                                     |`)
+    console.log (`|   3 - Apagar                                       |`)
+    console.log (`|   4 - Sair                                         |`)
+    console.log (`|----------------------------------------------------|`)
+     
+    }
+     let proximaTela = 0
+
+
+
+while (proximaTela !== 4) {
+    telaInicial();
+    proximaTela = parseInt(prompt(`Para onde você deseja ir? `));
+  
+
+    if (proximaTela === 1) {
+    cadastroItem();
+    }
+
+    else if (proximaTela === 2) {
+    listagem();
+    }
+    else if (proximaTela === 3) {
+    apagarItem();
+    }
+    else if (proximaTela === 4)
+        console.log (`Saindo do sistema`)
+    
+    else {
+    console.log("Opção errada! ");
+    prompt((`Digite Novamente`))
+    }
+}   
+
+
+
+
+function cadastroItem () { 
+    
+    let i = Vazio();
+    
+    nomeItem[i] = prompt(`Digite o nome do novo item: `)
     precoItem[i] = parseFloat(prompt(`Digite o preço do item ${i + 1}: `))
     estoque[i] = parseInt(prompt(`Digite a quantidade em estoque do item ${i + 1}: `))
     console.clear()
@@ -27,67 +95,56 @@ else if (precoItem[i] >= 100 && precoItem[i] < 500){
 else {
     raridade[i] = `Lendário`
 };
+    prompt(`Pressione ENTER para continuar`)
 }
 
-function EspadaDeFogo () { // Função com os dados fixos da espada
-    const nomeItem = `Espada de fogo rasga céus`
-    const precoItem = 400
-    const valoritem = `False`
-    const raridade = `Raro`
-    const estoque = 5
-    const disponibilidade = `Está disponível`
-    
+function apagarItem (){
 
+    let apagarID = parseInt(prompt(`Digite o ID que você deseja excluir: `))
 
-    console.log(`|--------------FICHA DE ITENS-----------------|\n`)
-    console.log(`Nome do item:  ${nomeItem}`)
-    console.log(`Preço do item: ${precoItem}`)
-    console.log(`Item de alto valor (acima de 500)? ${valoritem}`)
-    console.log(`Raridade do item: ${raridade}`)
-    console.log(`Quantidade em estoque: ${estoque}\n`)
-    console.log(`Disponibilidade do item: ${disponibilidade}\n`) 
+    let indice = apagarID
 
-}
-// Mostra as informações dos itens
-function FichaDeItens() {
-  
-    EspadaDeFogo()
-
-    for (let i = 0; i < 4; i++){
-    // Verifica se o item custa maior que 500
-    let valoritem = precoItem[i] > 500 ? `True` : `False`
-    // Verifica se o item possui estoque e preço válido
-    let disponibilidade = estoque[i] > 0 && precoItem[i] > 0 ? `Está disponível` : `Não está disponível`
-
-    console.log(`|--------------FICHA DE ITENS-----------------|\n`)
-    console.log(`Nome do item:  ${catalogo_itens[i]}`)
-    console.log(`Preço do item: ${precoItem[i]}`)
-    console.log(`Item de alto valor (acima de 500)? ${valoritem}`)
-    console.log(`Raridade do item: ${raridade[i]}`)
-    console.log(`Quantidade em estoque: ${estoque[i]}\n`)
-    console.log(`Disponibilidade do item: ${disponibilidade}\n`)
-    }
-}
-
-FichaDeItens()
-
-console.log (`|--------------REGRISTRO DE ITENS-----------------|\n`)
-
-
-console.log (`O item Espada de fogo rasga céus foi cadastrado com sucesso!`)
-
-for (let i = 0; i < 4; i++) { // Caso itens possua um índice par, ele estará em promoção, caso contrário, o item foi cadastrado com sucesso
-    if (i % 2 === 0) {
-        console.log(`O item ${nomeItem[i]} esta  em promoçao nessa semana!.`)
+    if (indice >= 0 && (indice in nomeItem)){
+        console.log (`Deseja realmente apagar o item ${nomeItem[indice]} ? `)
+        let confirmacao = prompt("Responda com S ou N").toUpperCase()
         
-        }
-        else {
-            console.log(`O item ${nomeItem[i]} foi cadastrado com sucesso!`)
-    }
-}    
+        
+        if (confirmacao === `S`){
+            delete nomeItem[indice]
+            delete precoItem[indice]
+            delete estoque[indice]
+            delete raridade[indice]
+            delete id[indice]
 
-console.log(`\n|-------------- Itens da loja ---------------|\n`)
-// Exibe os itens do catálogo
-for (const item of catalogo_itens) {
-    console.log(`Item: ${item}`);
-}   
+            console.log (`Item removido`)
+        }
+
+            
+        else {
+            console.log(`Operação cancelada`)
+        }
+    }    
+    prompt(`Pressione ENTER para continuar`)
+}
+
+function listagem() {
+    console.clear();
+    console.log(`|===========================================================================|`);
+    console.log(`|                ITENS NO ESTOQUE                                           |`);
+    console.log(`|===========================================================================|`);
+    
+    if (estoque.length === 0) {
+        console.log(`| O estoque está completamente vazio.                |`);
+    } else {
+        for (let i = 0; i < nomeItem.length; i++) {
+            if (i in nomeItem) {
+                console.log(`| ID: ${i + 1} | ${nomeItem[i]} | Preço: R$ ${precoItem[i]} | Estoque: ${estoque[i]} unidade | Raridade: ${raridade[i]}`);
+        }
+    }  
+}
+  
+    console.log(`|===========================================================================|`);
+    
+    
+    prompt('\nPressione ENTER para voltar ao Menu Principal');
+} 
