0aa26dd28add56703737f90287ad473463daf2bd
gabriel-de-leao <gabriel_de-leao@estudante.sesisenai.org.br>
2026-09-23 13:58:47 -0300

    lista de exercícios

---
+++ listaExercicios.js/exercicio14.js
@@ -0,0 +1,12 @@
+const calcularTotal = function(precos) {
+let total = 0
+
+  for (const preco of precos) {
+    total += preco
+  }
+
+return total
+}
+
+const listaPrecos = [10, 5, 10 , 10]
+console.log (`Resultado: ${calcularTotal(listaPrecos)}`)
